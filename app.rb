486ea8639f905c7e5f0c10e39206d7f94fdf6fc6
Sandy Pounoussamy <pounoussamy.sandy@gmail.com>
2021-02-16 15:45:42 +0000

    Adding params

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Misty", "Almond"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  @last_name = params[:last_name]
+  @age = params[:age]
   erb(:index)
 end
